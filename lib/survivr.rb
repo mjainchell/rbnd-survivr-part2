dd9dcc00165b47ec5705b5270b0d13261a01478e
Mark Jainchell <mjainchell@yahoo.com>
2016-08-11 15:40:17 -0400

    All code runs without error

--- lib/survivr.rb
+++ lib/survivr.rb
@@ -2,41 +2,58 @@ require_relative "game"
 require_relative "tribe"
 require_relative "contestant"
 require_relative "jury"
+require "colorizr.rb"
 
 #After your tests pass, uncomment this code below
 #=========================================================
-# # Create an array of twenty hopefuls to compete on the island of Borneo
-# @contestants = %w(carlos walter aparna trinh diego juliana poornima juha sofia julia fernando dena orit colt zhalisa farrin muhammed ari rasha gauri)
-# @contestants.map!{ |contestant| Contestant.new(contestant) }.shuffle!
+ # Create an array of twenty hopefuls to compete on the island of Borneo
+ @contestants = %w(carlos walter aparna trinh diego juliana poornima juha sofia julia fernando dena orit colt zhalisa farrin muhammed ari rasha gauri)
+ @contestants.map!{ |contestant| Contestant.new(contestant) }.shuffle!
+
+ # Create two new tribes with names
+ @coyopa = Tribe.new(name: "Pagong".red, members: @contestants.shift(10))
+ @hunapu = Tribe.new(name: "Tagi".green, members: @contestants.shift(10))
 #
-# # Create two new tribes with names
-# @coyopa = Tribe.new(name: "Pagong", members: @contestants.shift(10))
-# @hunapu = Tribe.new(name: "Tagi", members: @contestants.shift(10))
-#
-# # Create a new game of Survivor
-# @borneo = Game.new(@coyopa, @hunapu)
+ # Create a new game of Survivor
+ @borneo = Game.new(@coyopa, @hunapu)
 #=========================================================
 
 
 #This is where you will write your code for the three phases
 def phase_one
+  puts 'Phase One'
+  8.times {
+    @borneo.immunity_challenge.tribal_council
+  }
 end
 
+@jury = Jury.new
+
 def phase_two
+  puts 'Phase Two'
+  3.times {
+    selected = @borneo.individual_immunity_challenge
+    @jury.add_member(selected)
+  }
 end
 
 def phase_three
+  puts 'Phase Three'
+  7.times {
+    eliminated = @borneo.individual_immunity_challenge
+    @jury.add_member(eliminated)
+  }
 end
 
 
 # If all the tests pass, the code below should run the entire simulation!!
 #=========================================================
-# phase_one #8 eliminations
-# @merge_tribe = @borneo.merge("Cello") # After 8 eliminations, merge the two tribes together
-# phase_two #3 more eliminations
-# @jury = Jury.new
-# phase_three #7 elminiations become jury members
-# finalists = @merge_tribe.members #set finalists
-# vote_results = @jury.cast_votes(finalists) #Jury members report votes
-# @jury.report_votes(vote_results) #Jury announces their votes
-# @jury.announce_winner(vote_results) #Jury announces final winner
+ phase_one #8 eliminations
+ @merge_tribe = @borneo.merge("Cello".blue) # After 8 eliminations, merge the two tribes together
+ phase_two #3 more eliminations
+ @jury = Jury.new
+ phase_three #7 elminiations become jury members
+ finalists = @merge_tribe.members #set finalists
+ vote_results = @jury.cast_votes(finalists) #Jury members report votes
+ @jury.report_votes(vote_results) #Jury announces their votes
+ @jury.announce_winner(vote_results) #Jury announces final winner
--- lib/tribe.rb
+++ lib/tribe.rb
@@ -8,12 +8,19 @@ class Tribe
     puts "The Tribe: #{tribe_hash[:name]} has been created!"
   end
 
-  def tribal_council(immune_member)
-    @immune = immune_member[:immune]
-    @immune_array = [@members, @immune]
-    @immune_array[0].pop if @immune_array[0].include?(@immune)
-  end
+  #def tribal_council(immune_member
+   # @immune = immune_member[:immune]
+   # @immune_array = [@members, @immune]
+  #  @immune_array[0].pop if @immune_array[0].include?(@immune)
+ # end
 
+  # another tribal_council method, code block provided by @fuzz
+
+  def tribal_council(options = {})
+    loser = @members.select { |member| member != options[:immune] }.sample
+    puts "#{@name} has voted off " + "#{loser}"+ "."
+    @members.delete(loser)
+  end
 
 
 end
